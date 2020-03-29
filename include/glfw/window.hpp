e688fa45547c454435c57ee5d33896f2b218a5e9
Damon Tarry <drt1245@gmail.com>
2020-03-28 21:47:26 -0700

    use unique_ptr to hold GLFWwindow, to fix move constructor/assignment

--- include/glfw/window.hpp
+++ include/glfw/window.hpp
@@ -8,23 +8,21 @@
 #include <glfw/monitor.hpp>
 #include <glfw/window_attribute.hpp>
 
+#include <memory>
+
 namespace glfw {
 
 struct window final {
-  window() : w{nullptr} {}
+  window() : w{nullptr, c::glfwDestroyWindow} {}
   window(frame_size<int> s, const char * const title,
          glfw::monitor m = glfw::monitor{}, window const& share = window{})
-      : w{nullptr}, poll_events_{true}, wait_events_{false} {
-    w = c::glfwCreateWindow(s.width, s.height, title, m.get(),
-                            const_cast<c::GLFWwindow*>(share.get()));
-    detail::handle_glfw_error(w, "Couldn't create window!");
-    q = environment::register_window(w);
+      : w{c::glfwCreateWindow(s.width, s.height, title, m.get(), const_cast<c::GLFWwindow*>(share.get())), c::glfwDestroyWindow}, poll_events_{true}, wait_events_{false} {
+    detail::handle_glfw_error(w.get(), "Couldn't create window!");
+    q = environment::register_window(w.get());
     make_current();
   }
 
-  ~window() noexcept {
-    if (w) { c::glfwDestroyWindow(w); }
-  }
+  ~window() noexcept {}
 
   window(window&&) = default;
   window& operator=(window&&) = default;
@@ -42,19 +40,14 @@ struct window final {
     if (poll_events_) {
       poll_events();
     }
-    return !c::glfwWindowShouldClose(w);
+    return !c::glfwWindowShouldClose(w.get());
   }
 
-  auto get() noexcept -> c::GLFWwindow * { return w; }
-  auto get() const noexcept -> c::GLFWwindow const * { return w; }
-  auto release() -> c::GLFWwindow * {
-    auto tmp = w;
-    w = nullptr;
-    return tmp;
-  }
+  auto get() noexcept -> c::GLFWwindow * { return w.get(); }
+  auto get() const noexcept -> c::GLFWwindow const * { return w.get(); }
 
  private:
-  c::GLFWwindow* w;
+  std::unique_ptr<c::GLFWwindow, decltype(&c::glfwDestroyWindow)> w;
   event_queue* q;
   bool poll_events_;
   bool wait_events_;
@@ -73,7 +66,7 @@ struct window final {
   int attribute(window_attribute a) const noexcept {
     assert_non_empty_window();
     auto v = c::glfwGetWindowAttrib(
-     w, static_cast<std::underlying_type_t<window_attribute>>(a));
+     w.get(), static_cast<std::underlying_type_t<window_attribute>>(a));
     if (v == 0) {
       report_runtime_error("Couldn't get attribute " + to_string(a) + ".");
     }
@@ -82,7 +75,7 @@ struct window final {
 
   void make_current() noexcept {
     assert_non_empty_window();
-    c::glfwMakeContextCurrent(w);
+    c::glfwMakeContextCurrent(w.get());
   }
 
   void set_wait_events(bool b) { wait_events_ = b; }
@@ -93,93 +86,92 @@ struct window final {
 
   bool is_closed() const noexcept {
     assert_non_empty_window();
-    return bool{w};
+    return w.get() != nullptr;
   }
 
   void on_close(c::GLFWwindowclosefun callback) const noexcept {
     assert_non_empty_window();
-    c::glfwSetWindowCloseCallback(w, callback);
+    c::glfwSetWindowCloseCallback(w.get(), callback);
   }
 
   void swap_buffers() noexcept {
     assert_non_empty_window();
-    c::glfwSwapBuffers(w);
+    c::glfwSwapBuffers(w.get());
   }
 
   void resize(frame_size<int> s) noexcept {
     assert_non_empty_window();
-    c::glfwSetWindowSize(w, s.width, s.height);
+    c::glfwSetWindowSize(w.get(), s.width, s.height);
   }
 
   frame_size<int> size() const noexcept {
     assert_non_empty_window();
     frame_size<int> s_;
-    c::glfwGetWindowSize(w, &s_.width, &s_.height);
+    c::glfwGetWindowSize(w.get(), &s_.width, &s_.height);
     return s_;
   }
 
   frame_size<int> framebuffer_size() const noexcept {
     assert_non_empty_window();
     frame_size<int> s_;
-    c::glfwGetFramebufferSize(w, &s_.width, &s_.height);
+    c::glfwGetFramebufferSize(w.get(), &s_.width, &s_.height);
     return s_;
   }
 
   void position(frame_coordinates<int> p) noexcept {
     assert_non_empty_window();
-    c::glfwSetWindowPos(w, p.x, p.y);
+    c::glfwSetWindowPos(w.get(), p.x, p.y);
   }
 
   frame_coordinates<int> position() const noexcept {
     assert_non_empty_window();
     frame_coordinates<int> pos;
-    c::glfwGetWindowPos(w, &pos.x, &pos.y);
+    c::glfwGetWindowPos(w.get(), &pos.x, &pos.y);
     return pos;
   }
 
   void title(const char* new_title) noexcept {
     assert_non_empty_window();
-    c::glfwSetWindowTitle(w, new_title);
+    c::glfwSetWindowTitle(w.get(), new_title);
   }
 
   void minimize() noexcept {
     assert_non_empty_window();
-    c::glfwIconifyWindow(w);
+    c::glfwIconifyWindow(w.get());
   }
   void restore() noexcept {
     assert_non_empty_window();
-    c::glfwRestoreWindow(w);
+    c::glfwRestoreWindow(w.get());
   }
   void show() noexcept {
     assert_non_empty_window();
-    c::glfwShowWindow(w);
+    c::glfwShowWindow(w.get());
   }
   void hide() noexcept {
     assert_non_empty_window();
-    c::glfwHideWindow(w);
+    c::glfwHideWindow(w.get());
   }
 
   void close() noexcept {
     assert_non_empty_window();
-    c::glfwDestroyWindow(w);
-    w = nullptr;
+    w.reset(nullptr);
   }
 
   void mouse_position(frame_coordinates<double> p) noexcept {
     assert_non_empty_window();
-    c::glfwSetCursorPos(w, p.x, p.y);
+    c::glfwSetCursorPos(w.get(), p.x, p.y);
   }
 
   frame_coordinates<double> mouse_position() const noexcept {
     assert_non_empty_window();
     frame_coordinates<double> p;
-    c::glfwGetCursorPos(w, &p.x, &p.y);
+    c::glfwGetCursorPos(w.get(), &p.x, &p.y);
     return p;
   }
 
   glfw::monitor monitor() const noexcept {
     assert_non_empty_window();
-    return glfw::monitor{c::glfwGetWindowMonitor(w)};
+    return glfw::monitor{c::glfwGetWindowMonitor(w.get())};
   }
 
   void make_context_current() noexcept {
