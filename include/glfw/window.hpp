d4257639629ea84758ef83df5df248ce84ad9056
Damon Tarry <drt1245@gmail.com>
2020-03-28 21:59:11 -0700

    make window more RAII

--- include/glfw/window.hpp
+++ include/glfw/window.hpp
@@ -13,10 +13,9 @@
 namespace glfw {
 
 struct window final {
-  window() : w{nullptr, c::glfwDestroyWindow} {}
   window(frame_size<int> s, const char * const title,
-         glfw::monitor m = glfw::monitor{}, window const& share = window{})
-      : w{c::glfwCreateWindow(s.width, s.height, title, m.get(), const_cast<c::GLFWwindow*>(share.get())), c::glfwDestroyWindow}, poll_events_{true}, wait_events_{false} {
+         glfw::monitor m = glfw::monitor{}, window * share = nullptr)
+      : w{c::glfwCreateWindow(s.width, s.height, title, m.get(), share ? share->get() : nullptr), c::glfwDestroyWindow}, poll_events_{true}, wait_events_{false} {
     detail::handle_glfw_error(w.get(), "Couldn't create window!");
     q = environment::register_window(w.get());
     make_current();
@@ -31,9 +30,6 @@ struct window final {
   window& operator=(window const&) = delete;
 
   explicit operator bool() {
-    if(!w) {
-      return false;
-    }
     if (wait_events_) {
       wait_events();
     }
@@ -51,9 +47,6 @@ struct window final {
   event_queue* q;
   bool poll_events_;
   bool wait_events_;
-  void assert_non_empty_window() const noexcept {
-    assert(w && "action on empty window!");
-  }
 
  public:
   static void hint(window_attribute target, int hint) noexcept {
@@ -64,7 +57,6 @@ struct window final {
   static void default_hints() noexcept { c::glfwDefaultWindowHints(); }
 
   int attribute(window_attribute a) const noexcept {
-    assert_non_empty_window();
     auto v = c::glfwGetWindowAttrib(
      w.get(), static_cast<std::underlying_type_t<window_attribute>>(a));
     if (v == 0) {
@@ -74,7 +66,6 @@ struct window final {
   }
 
   void make_current() noexcept {
-    assert_non_empty_window();
     c::glfwMakeContextCurrent(w.get());
   }
 
@@ -84,100 +75,73 @@ struct window final {
   void set_poll_events(bool b) { poll_events_ = b; }
   void poll_events() const { c::glfwPollEvents(); }
 
-  bool is_closed() const noexcept {
-    assert_non_empty_window();
-    return w.get() != nullptr;
-  }
-
   void on_close(c::GLFWwindowclosefun callback) const noexcept {
-    assert_non_empty_window();
     c::glfwSetWindowCloseCallback(w.get(), callback);
   }
 
   void swap_buffers() noexcept {
-    assert_non_empty_window();
     c::glfwSwapBuffers(w.get());
   }
 
   void resize(frame_size<int> s) noexcept {
-    assert_non_empty_window();
     c::glfwSetWindowSize(w.get(), s.width, s.height);
   }
 
   frame_size<int> size() const noexcept {
-    assert_non_empty_window();
     frame_size<int> s_;
     c::glfwGetWindowSize(w.get(), &s_.width, &s_.height);
     return s_;
   }
 
   frame_size<int> framebuffer_size() const noexcept {
-    assert_non_empty_window();
     frame_size<int> s_;
     c::glfwGetFramebufferSize(w.get(), &s_.width, &s_.height);
     return s_;
   }
 
   void position(frame_coordinates<int> p) noexcept {
-    assert_non_empty_window();
     c::glfwSetWindowPos(w.get(), p.x, p.y);
   }
 
   frame_coordinates<int> position() const noexcept {
-    assert_non_empty_window();
     frame_coordinates<int> pos;
     c::glfwGetWindowPos(w.get(), &pos.x, &pos.y);
     return pos;
   }
 
   void title(const char* new_title) noexcept {
-    assert_non_empty_window();
     c::glfwSetWindowTitle(w.get(), new_title);
   }
 
   void minimize() noexcept {
-    assert_non_empty_window();
     c::glfwIconifyWindow(w.get());
   }
   void restore() noexcept {
-    assert_non_empty_window();
     c::glfwRestoreWindow(w.get());
   }
   void show() noexcept {
-    assert_non_empty_window();
     c::glfwShowWindow(w.get());
   }
   void hide() noexcept {
-    assert_non_empty_window();
     c::glfwHideWindow(w.get());
   }
 
-  void close() noexcept {
-    assert_non_empty_window();
-    w.reset(nullptr);
-  }
-
   void mouse_position(frame_coordinates<double> p) noexcept {
-    assert_non_empty_window();
     c::glfwSetCursorPos(w.get(), p.x, p.y);
   }
 
   frame_coordinates<double> mouse_position() const noexcept {
-    assert_non_empty_window();
     frame_coordinates<double> p;
     c::glfwGetCursorPos(w.get(), &p.x, &p.y);
     return p;
   }
 
   glfw::monitor monitor() const noexcept {
-    assert_non_empty_window();
     return glfw::monitor{c::glfwGetWindowMonitor(w.get())};
   }
 
   void make_context_current() noexcept {
-    if (w) { make_current(); } else {
-      c::glfwMakeContextCurrent(nullptr);
-    }
+    make_current();
   }
 
   bool poll(event& e) noexcept {
